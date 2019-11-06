035bf006bba95c85e784596b2a72319b502c5b5c
Vedant <vshetty.vs@gmail.com>
2019-11-06 11:47:53 +0530

    Added turn limit and formatting

--- Ruby On Rails/Sinatra/web_guesser/number_guessing_game.rb
+++ Ruby On Rails/Sinatra/web_guesser/number_guessing_game.rb
@@ -1,26 +1,41 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-NUMBER = rand(100)
+@@number = rand(100)
+@@guesses = 5
+
 get '/numberGuesser' do
+  @@guesses -= 1
   user_guess = params['guess']
-  message = check_guess(user_guess)
+  message = check_guess(user_guess,@@number)
+  if message == 'You got it right!'
+    reset
+  elsif @@guesses == 0
+    message = "You lost. The number was #{@@number}! A new Number has been generated"
+    reset
+  end
   cheat = params['cheat'] == 'true'
-  erb :index, locals: { number: NUMBER, message: message, cheat_mode: cheat }
+  erb :index, locals: { number: @@number, message: message, cheat_mode: cheat, 
+                        turns:@@guesses }
 end
 
-def check_guess(user_guess)
+def reset
+  @@number = rand(100)
+  @@guesses = 5
+end
+
+def check_guess(user_guess,number)
   return '' if user_guess.nil?
 
   user_guess = user_guess.to_i
 
-  return 'You got it right!' if user_guess - NUMBER == 0
+  return 'You got it right!' if (user_guess - number).zero?
 
-  return  'Too high!' if (user_guess - NUMBER).between?(1,5)
+  return  'Too high!' if (user_guess - number).between?(1, 5)
 
-  return 'Too low!' if (user_guess - NUMBER).between?(-5,-1)
+  return 'Too low!' if (user_guess - number).between?(-5, -1)
 
-  return 'Way too high!' if (user_guess - NUMBER) > 5
+  return 'Way too high!' if (user_guess - number) > 5
 
   'Way too low!'
 end
