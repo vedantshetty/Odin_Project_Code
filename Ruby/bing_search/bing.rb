bc86d5a95a6a3a85328f971fcc11ae76ac061fda
Vedant <vshetty.vs@gmail.com>
2019-11-08 14:54:28 +0530

    ruby bing search implemented

---
+++ Ruby/bing_search/bing.rb
@@ -0,0 +1,34 @@
+require 'rest-client'
+
+class Bing
+  def search(search_term=input)
+    base = 'https://www.bing.com/search'
+    response = RestClient.get(base, {params:{:q =>search_term }})
+
+    if response.code == 200
+      puts response.body
+    else
+      puts 'error'
+    end
+  end
+
+  def format(search)
+    search.split(' ').join('+')
+  end
+  private
+  def input 
+    puts 'Enter the search term'
+    format(gets.chomp)
+  end
+end
+
+
+B = Bing.new
+B.search('Lebron James')
+puts '----------------------------'
+puts 
+puts
+
+
+B.search
+
