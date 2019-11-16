8d95fb4eeb6e158cb7bfc152e6aed0854fb3501a
Vedant <vshetty.vs@gmail.com>
2019-11-16 10:25:16 +0530

    Impletemented sessions for mastermind sinatra

--- Ruby On Rails/Sinatra/mastermind/application.rb
+++ Ruby On Rails/Sinatra/mastermind/application.rb
@@ -1,19 +1,26 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require_relative 'lib/Mastermind'
-
+enable :sessions
 helpers do
   def lose
-    @@turn =1
   end
 end
 
-@@turn = 0
 get '/play' do
-  @@turn+=1
-  lose if @@turn ==13
 
-  erb :game, :locals =>{:turn=> @turn}
+  if params['color1']
+   session[:colors].push([params['color1'],params['color2'],params['color3'],params['color4']])
+    session[:matches].push(session[:game].feedback(session[:colors][-1]))
+    session[:turns]+=1
+  else
+    session[:turns] = 1
+    session[:colors] =[]
+    session[:matches] = []
+    session[:game] = Mastermind.new
+  end
+  #Write Code for running out of turn
+  erb :game, :locals =>{:turn => session[:turns], :codes => session[:colors], :matches => session[:matches]}
 end
 
 
--- Ruby On Rails/Sinatra/mastermind/lib/Mastermind.rb
+++ Ruby On Rails/Sinatra/mastermind/lib/Mastermind.rb
@@ -1,6 +1,4 @@
 # Guess opponents secret code within a certain number of turns.
-
-# TODO - Create AI that can guess the user generated code
 class Mastermind
   def initialize(number = 4, mode = 'codebreaker')
     @code = []
@@ -10,36 +8,16 @@ class Mastermind
     when 'codebreaker'
       codemaker
     when 'codemaker'
-      #input_code
       codebreaker
     end
-  end
-
-  def input_code
-    puts 'Enter your secret code'
-    puts ' Enter you colors through a sequence of their codes'\
-    'e.g:5,5,4,6 for Black, Black, Yellow, White'
-    @code = input
   end
 
   def codemaker
     (0...@number).each do |peg|
       @code[peg] = @colors[rand(6)]
     end
-    guide_codebreaker
-  end
-
-  def begin
-    12.times do |index|
-      puts "TURN #{index}".center(50,'*')
-      user_code = input
-      matches = feedback(user_code)
-      if win?(matches[0])
-        puts 'Congrats!! You are a mastermind. You correctly guessed the code'
-        print_code
-      end
-    end
-    lose
+    puts 'new code'
+    puts @code
   end
 
   def win?(matches)
@@ -48,48 +26,12 @@ class Mastermind
     true
   end
 
-  def lose
-    puts 'Sorry, you ran out of turns'
-    puts 'The correct code was'
-    print_code
-    puts 'Better luck next time:)'
-  end
-
-  def print_code
-    puts @code.join(', ')
-  end
-
-  def illegal(code)
-    return false if code.length == @number && code.none?(nil)
-
-    if code.length != @number
-      print 'The code length is incorrect'
-    else
-      print 'One or more of your color codes are wrong'
-    end
-    puts '. Please try again.'
-    true
-  end
-
-  def input
-    puts 'Enter your code below'
-    print_color_codes
-    user_code = ''
-    loop do
-      user_code = gets.chomp.split(',').map do |code|
-        @colors[code.to_i - 1] if code.to_i.between?(1, 6)
-      end
-      break unless illegal(user_code)
-    end
-    user_code
-  end
-
   def feedback(user_code)
+    user_code = user_code.dup
     temp_code = @code.dup
     exact_match = 0
     color_match = 0
     temp_code.each_with_index do |element, index|
-      puts user_code[index]
       if element == user_code[index]
         exact_match += 1
         temp_code[index], user_code[index] = nil
@@ -106,27 +48,7 @@ class Mastermind
         end
       end
     end
-    puts "There were #{exact_match} exact matches and #{color_match} color matches"
     [exact_match, color_match]
-  end
-
-  def print_color_codes
-    puts 'COLORS-->'
-    @colors.each_with_index do |color,code|
-      puts "#{code+1}: #{color}"
-    end
-  end
-
-  def guide_codebreaker
-    puts 'guide'.upcase.center(50,'*')
-    puts "\n1. Enter your colors through a sequences of their codes"
-    puts 'e.g: 5,5,4,6 for Black, Black, Yellow, White'
-
-    puts "\n2. Each turn the computer will provide feedback"\
-         ' on how close you were to the actual answer'
-
-    puts "\n3. You have 12 turns to correctly guess the code"
-    puts ''.center(50, '*')
   end
 end
 
